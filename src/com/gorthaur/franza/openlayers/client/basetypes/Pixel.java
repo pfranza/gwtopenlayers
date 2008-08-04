f30479ab732b1999bb20f4b0d63f14d826192cab
pfranza <pfranza@177b6d9f-9253-0410-9884-55dedcbf4efb>
2008-08-04 03:02:38 +0000

    implements most of the bounds object

---
+++ src/com/gorthaur/franza/openlayers/client/basetypes/Pixel.java
@@ -0,0 +1,31 @@
+package com.gorthaur.franza.openlayers.client.basetypes;
+
+public class Pixel {
+
+	private int x, y;
+
+	public Pixel(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+	public final int getX() {
+		return x;
+	}
+
+	public final void setX(int x) {
+		this.x = x;
+	}
+
+	public final int getY() {
+		return y;
+	}
+
+	public final void setY(int y) {
+		this.y = y;
+	}
+	
+	
+	
+}
