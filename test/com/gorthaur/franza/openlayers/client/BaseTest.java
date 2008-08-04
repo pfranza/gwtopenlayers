abe391b6535463e7557159ca6125185b210a004f
pfranza <pfranza@177b6d9f-9253-0410-9884-55dedcbf4efb>
2008-08-04 10:49:00 +0000

    TestCase for the Pixel Class

---
+++ test/com/gorthaur/franza/openlayers/client/BaseTest.java
@@ -0,0 +1,16 @@
+package com.gorthaur.franza.openlayers.client;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+public class BaseTest extends GWTTestCase {
+
+
+	public String getModuleName() {
+		return "com.gorthaur.franza.openlayers.OpenLayers";
+	}
+
+	public void testTrue() throws Exception {
+		assertTrue(true);
+	}
+	
+}
