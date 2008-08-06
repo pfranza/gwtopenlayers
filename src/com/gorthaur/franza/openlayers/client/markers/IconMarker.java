0af8cb3e50a1f7ec537253f5c26be52d22c678dc
pfranza <pfranza@177b6d9f-9253-0410-9884-55dedcbf4efb>
2008-08-06 18:29:06 +0000

    added marker mouse listeners

--- src/com/gorthaur/franza/openlayers/client/markers/IconMarker.java
+++ src/com/gorthaur/franza/openlayers/client/markers/IconMarker.java
@@ -8,11 +8,24 @@ public class IconMarker extends Marker{
 
 	public IconMarker(Icon icon, LonLat ll) {
 		super(_newInstance(icon.getIconClone(), ll.getLongitude(), ll.getLatitude())); 
+		_registerListeners(this, getJsObject());
 	}
+
+	private native void _registerListeners(IconMarker m, JavaScriptObject marker) /*-{
+		marker.events.register("mousedown", null, function clickEvent(evt) {
+			m.@com.gorthaur.franza.openlayers.client.markers.IconMarker::fireClickEvent()();
+        });
+       	marker.events.register("mouseover", null, function overEvent(evt) {
+			m.@com.gorthaur.franza.openlayers.client.markers.IconMarker::fireHoverOverEvent()();
+        }); 
+        marker.events.register("mouseout", null, function overEvent(evt) {
+			m.@com.gorthaur.franza.openlayers.client.markers.IconMarker::fireHoverOutEvent()();
+        });
+	}-*/;
 
 	private native static JavaScriptObject _newInstance(JavaScriptObject icon,
 			double lon, double lat) /*-{
-		return new $wnd.OpenLayers.Marker(new $wnd.OpenLayers.LonLat(lon,lat),icon)
+		return new $wnd.OpenLayers.Marker(new $wnd.OpenLayers.LonLat(lon,lat),icon);
 	}-*/;
 	
 }
